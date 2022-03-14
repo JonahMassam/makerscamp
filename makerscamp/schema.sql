54dea20356aec7ed7a19cccbc3ec34c4cb89bc49
Jonah Massam <Jonah.Massam10@yahoo.co.uk>
2022-03-14 15:23:31 +0000

    Init commit

---
+++ makerscamp/schema.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS channels;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS user_channels;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username text NOT NULL
+);
+
+CREATE TABLE channels (
+    id SERIAL PRIMARY KEY,
+    name text NOT NULL
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    user_id int4 NOT NULL,
+    message text NOT NULL,
+    channel_id int4 NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (channel_id) REFERENCES channels (id)
+);
+
+CREATE TABLE user_channels (
+    id SERIAL PRIMARY KEY,
+    user_id int4 NOT NULL,
+    channel_id int4 NOT NULL,
+    FOREIGN KEY (channel_id) REFERENCES channels (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
