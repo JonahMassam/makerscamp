eca20e5ae32f522ddd15c546d9c20a6c20833e21
T Henebury <tiffsisstracker@gmail.com>
2022-03-15 16:53:54 +0000

    unfinished additions

--- makerscamp/classes/users.py
+++ makerscamp/classes/users.py
@@ -1,6 +1,10 @@
+from makerscamp.db import get_db
+
 
 class Users():
     
     @classmethod
     def create(cls, username)
-    db.get_db
+    with get_db.cursor() as cur
+        cur.execute('INSERT INTO users (username)' 'VALUES ?', (username))
+        db.commit()
--- tests/unittests/test_users.py
+++ tests/unittests/test_users.py
@@ -1,11 +1,4 @@
 from makerscamp.classes import Users
-import os
-import psycopg2
-
-conn = psycopg2.connect(
-        host="localhost",
-        database="",
-        user=os.environ['tiffany-morrigan'],
 
 class TestUsers():
     """Testing for the Users class"""
@@ -24,22 +17,22 @@ class TestUsers():
         print(users_array)
         assert username in users_array
 
-    def test_find_user():
-        """a user can be retrieved from the database by username"""
-
-        # find a username within a database
-        name = "Joe Bloggs"
-
-        # return User object with username property
-        assert Users.find(name).username == username
-
-        
-
-    def test_find_by_id():
-        """a user can be retrieved from the database"""
-
-    def test_find_all():
-        """all users can be retrieved from the database"""
-
-    def test_authenticate():
-        """a user can be authenticated""
+#    def test_find_user():
+#        """a user can be retrieved from the database by username"""
+#
+#        # find a username within a database
+#        name = "Joe Bloggs"
+#
+#        # return User object with username property
+#        assert Users.find(name).username == username
+#
+#        
+#
+#    def test_find_by_id():
+#        """a user can be retrieved from the database"""
+#
+#    def test_find_all():
+#        """all users can be retrieved from the database"""
+#
+#    def test_authenticate():
+#        """a user can be authenticated""
