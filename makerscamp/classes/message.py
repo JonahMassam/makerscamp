661c27aaab5c72ca6678028e59e0343c65d384ca
T Henebury <tiffsisstracker@gmail.com>
2022-03-17 16:23:38 +0000

    unfinished message-class - switch from dataclass to regular class

--- makerscamp/classes/message.py
+++ makerscamp/classes/message.py
@@ -1,10 +1,17 @@
 from makerscamp.db import *
-from dataclasses import dataclass
 
-@dataclass
-class Message:
-    """Class to hold messages"""
-    id: str
-    user_id: str
-    channel_id: str
-    message: str
+db_conn = DB
+
+class Message():
+
+    def __init__(self, pk_id, user_id, channel_id, message):
+        self.pk_id = pk_id
+        self.user_id = user_id
+        self.channel_id = channel_id
+        self.message = message
+
+    @classmethod
+    def create(cls, id, user_id, channel_id, message):
+        """send from html form, through route, to sql"""
+        db_conn.exec(
+            f"INSERT INTO messages (user_id, channel_id, message) VALUES ('{user_id}', '{channel_id}', '{message}')")
