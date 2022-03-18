bba41576a1ba929660a97cb55f35a5c6221b5866
John Connolly <johnc1983@hotmail.co.uk>
2022-03-18 14:36:10 +0000

    retrieved data values pushed to test url

--- makerscamp/classes/message.py
+++ makerscamp/classes/message.py
@@ -1,10 +1,10 @@
-from makerscamp.db import *
+from makerscamp.classes.db import *
 
 db_conn = DB
 
 class Message():
 
-    def __init__(self, pk_id, user_id, channel_id, message):
+    def __init__(self, pk_id, user_id, message, channel_id):
         self.pk_id = pk_id
         self.user_id = user_id
         self.channel_id = channel_id
@@ -15,3 +15,18 @@ class Message():
         """send from html form, through route, to sql"""
         db_conn.exec(
             f"INSERT INTO messages (user_id, channel_id, message) VALUES ('{user_id}', '{channel_id}', '{message}')")
+
+    
+    @classmethod
+    def all(cls):
+        messages = db_conn.exec( f"SELECT * FROM messages")
+
+        return [
+            Message(
+                message[0],
+                message[1],
+                message[2],
+                message[3]
+            ) for message in messages 
+        ]
+    
