1162b246a38385d437629c5f35552a5cf718a661
T Henebury <tiffsisstracker@gmail.com>
2022-03-23 17:55:39 +0000

    added web_helper with login and register functions -- test login functionality

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -4,7 +4,13 @@ from faker import Faker
 fake = Faker()
 
 class TestRegister(BaseCase):
+    """Testing for registration functions"""
+
     def test_register_user(self):
+        """Provided correct input, check a new user account can register"""
+
+        # leaving below as is despite tests.web_helper
+        # more explicit for future debugging
         self.open("http://127.0.0.1:5000/auth/register")
         username = fake.name()
         password = fake.password()
@@ -16,6 +22,8 @@ class TestRegister(BaseCase):
         self.assert_text('Log In')
 
     def test_register_passwords_match(self):
+        """Provided mismatching passwords, check an error message appears"""
+
         self.open("http://127.0.0.1:5000/auth/register")
         username = fake.name()
         password = fake.password()
